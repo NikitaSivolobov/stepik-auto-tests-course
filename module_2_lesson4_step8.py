7d29c6a064774b6fb80652f42a6a82a8b41f952c
Nikita Sivolobov <nikita1314@yandex.ru>
2021-07-26 22:16:06 +0300

    homework from previous module

---
+++ module_2_lesson4_step8.py
@@ -0,0 +1,50 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+import time
+import math
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+try:
+    browser = webdriver.Chrome()
+    
+    browser.get("http://suninjuly.github.io/explicit_wait2.html")
+
+    # нажать кнопку price = 100
+    button = WebDriverWait(browser, 12).until(
+        EC.text_to_be_present_in_element((By.ID, "price"), "100")
+        )
+    
+    button1 = browser.find_element_by_id("book")
+    button1.click()
+
+    # решить капчу для роботов
+    x_element = browser.find_element_by_id("input_value")
+    x = x_element.text
+    y = calc(x)
+
+    #найденное число вставляем в текстовое поле
+    
+    input1 = browser.find_element_by_id("answer")
+    input1.send_keys(y)
+
+
+    button2 = browser.find_element_by_id("solve")
+    button2.click()
+
+       
+    # Проверяем, что смогли зарегистрироваться
+    # ждем загрузки страницы
+    time.sleep(1)
+
+    
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
+
